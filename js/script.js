a390f7aa592676a17437731b600dacefd02ffff6
Mami Hattori <humor_thx@icloud.com>
2025-06-01 15:31:09 +0900

    フォームのボタンの有効化処理がdebounceされるように修正

--- js/script.js
+++ js/script.js
@@ -182,12 +182,30 @@ document.addEventListener("DOMContentLoaded", function () {
       submitButton.disabled = !inputCheck;
     }
 
+    const debounce = (mainFunction, delay) => {
+      // Declare a variable called 'timer' to store the timer ID
+      let timer;
+
+      // Return an anonymous function that takes in any number of arguments
+      return function (...args) {
+        // Clear the previous timer to prevent the execution of 'mainFunction'
+        clearTimeout(timer);
+
+        // Set a new timer that will execute 'mainFunction' after the specified delay
+        timer = setTimeout(() => {
+          mainFunction(...args);
+        }, delay);
+      };
+    };
+
+    //debounceバージョンのcheckInputs
+    const debouncedCheckInputs = debounce(checkInputs, 300);
+
     inputs.forEach((input)=>{
-      input.addEventListener('input',checkInputs);
+      input.addEventListener('input',debouncedCheckInputs);
     });
 
     //バリデーションエラーでリダイレクト時の為に初期状態でも実行
     checkInputs();
   });
-
 });
